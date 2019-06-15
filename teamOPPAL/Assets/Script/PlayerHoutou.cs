fa0fe34e63539ed5b2933a76e403440ba1566e27
blue666225 <fwantazi1145@gmail.com>
2019-06-15 16:13:37 +0900

    Playerの砲塔の回転が途中

---
+++ teamOPPAL/Assets/Script/PlayerHoutou.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHoutou : MonoBehaviour
+{
+    Plane plane = new Plane();
+    float distance = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        plane.SetNormalAndPosition(Vector3.back, transform.localPosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //カメラとマウスの位置をもとにRayを用意
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //プレイヤーの高さにPlaneを更新して、カメラの情報をもとに
+        //地面の判定をして距離を取得
+        plane.SetNormalAndPosition(Vector3.up, transform.localPosition);
+        if (plane.Raycast(ray, out distance))
+        {
+            //距離をもとに交差を計算し、交点の方を向く
+            var lookPoint = ray.GetPoint(distance);
+            transform.LookAt(lookPoint);
+        }
+
+        
+
+    }
+
+    
+    
+
+}
