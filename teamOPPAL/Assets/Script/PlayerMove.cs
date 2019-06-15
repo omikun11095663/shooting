7ef0a891a358ace9f56131878c401c5229751d9e
blue666225 <fwantazi1145@gmail.com>
2019-06-15 14:51:46 +0900

    Playerの玉の発射の追加

--- teamOPPAL/Assets/Script/PlayerMove.cs
+++ teamOPPAL/Assets/Script/PlayerMove.cs
@@ -6,6 +6,9 @@ public class PlayerMove : MonoBehaviour
 {
     public float x;
     public float z;
+    public Vector3 direction;
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
